f8e7a3524fc94800cbcb332e507bce7b44c838f8
oskar wiiala <oskar.wiiala@metropolia.fi>
2021-09-23 19:32:24 +0300

    basic bottom navigation added alongside 2 fragments: tracker, statistics. clicking on either navigation button navigates to the corresponding fragment

--- app/src/main/java/com/georgv/sporttrackerapp/MainActivity.kt
+++ app/src/main/java/com/georgv/sporttrackerapp/MainActivity.kt
@@ -2,12 +2,47 @@ package com.georgv.sporttrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //this is a test just to see ig this is getting committed.
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.tracker -> {
+                    // Respond to navigation item 1 click
+                    navigateToTracker()
+                    true
+                }
+                R.id.statistics -> {
+                    // Respond to navigation item 2 click
+                    navigateToStatistics()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun navigateToTracker() {
+        Log.d("MainActivity.kt","BottomNavigation tracker clicked")
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace<TrackerFragment>(R.id.fragmentContainer)
+        transaction.commit()
+    }
+
+    private fun navigateToStatistics() {
+        Log.d("MainActivity.kt","BottomNavigation statistics clicked")
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace<StatisticsFragment>(R.id.fragmentContainer)
+        transaction.commit()
     }
 }
