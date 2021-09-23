014600b26e984f58e0e71e6381fdefdd4cdd2846
oskar wiiala <oskar.wiiala@metropolia.fi>
2021-09-23 17:42:39 +0300

    test

--- app/src/main/java/com/georgv/sporttrackerapp/MainActivity.kt
+++ app/src/main/java/com/georgv/sporttrackerapp/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //this is a test just to see ig this is getting committed.
     }
 }
